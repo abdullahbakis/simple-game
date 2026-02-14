60a72b4a21302b6a0666746d67511d7526e886b5
Abdullah Bakış <99887425+abdullahbakis@users.noreply.github.com>
2026-02-14 20:29:23 +0300

    Updated hazards.ts

--- src/game/hazards.ts
+++ src/game/hazards.ts
@@ -178,7 +178,7 @@ export function createHazards(
       x, y,
       radius: (80 + Math.random() * 30) * SCALE,
       killRadius: (22 + Math.random() * 6) * SCALE,
-      strength: 0.004 + Math.random() * 0.002,
+      strength: 0.006 + Math.random() * 0.003,
       angle: Math.random() * Math.PI * 2,
       speed: 0.001 + Math.random() * 0.0005,
     });
